2c906b8d313d0662fd8e7aaa1c8b7d9fb6961868
sajin bose <sajinbose8053@gmail.com>
2026-03-23 15:58:26 +0530

    added js for most functanality in the web paage

---
+++ static/js/app.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded" ,function(){
+    const searchInput = document.getElementById("searchInput");
+    const suggestionBox = document.getElementById("suggestionBox");
+
+    searchInput.addEventListener("keyup",function(){
+        const = search=searchInput.val
+
+})
