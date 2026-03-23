35d0836f8de286f51e9b1282b8745e6029bed37b
sajin bose <sajinbose8053@gmail.com>
2026-03-23 15:50:52 +0530

    added changes

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
 
-from flask import Flask ,render_template
-
+from flask import Flask ,render_template , request , redirect , session
 app = Flask(__name__)
+
+app.secret_key = "secret8053"
+
+
 products = [
         {
+            "id":1,
          "name":"Samsung S-26 Ultra",
          "image":"samsungs26.webp",
-         "description":"12gb ram ,256 gb rom |newly privacy feauture added |4+years os updates and security paches ",
+         "description":"12gb ram ,256 gb rom phone |newly privacy feauture added |4+years os updates and security paches ",
          "price":"120000",
          "categories":"elect"
          },
-        {
+        {   
+            "id":2,
             "name":"Oneplus bullet neck band",
             "image":"ONEplus neck band.jpg",
             "description":"12mm drivers|120hrs play back|ANC support ",
             "price":"5000",
             "categories":"elect"
             },
-        {
+        {   
+            "id":3,
             "name":"realme buds 5",
          "image":"realme buds5.webp",
          "description":"12mm drivers|120hrs play back|ANC support ",
@@ -26,48 +32,60 @@ products = [
          },
 
         {
+            "id":4,
          "name":"Oneplus powerbank 12000mah",
          "image":"OnePlus-150W-SuperVOOC.webp",
          "description":"fast charge support|2 device connectivity|military grade built quality ",
          "price":"4000",
          "categories":"elect"
          },
+
         {
+         "id":5,
          "name":"addidas boot",
          "image":"boot.jpg",
          "description":"easy to run|2 years warrenty |military grade built quality ",
          "price":"15000",
          "categories":"mens_fashion"
         },
+
         {
-            "name":"Vitamin C Scerum",
+         "id":6,
+         "name":"Vitamin C Scerum",
          "image":"vitaminC.webp",
          "description":"skin moisture|skin brightner |rich in vitamin c ",
          "price":"400",
          "categories":"beauty"
          },
-        {
+        {   
+            "id":7,
             "name":"wall clock",
             "image":"wall clock.webp",
             "description":"asthetic hoome|wall deceration + time   |home decors ",
             "price":"650",
             "categories":"decors"
             },
+
             {
+            "id":8,
             "name":"wall poster",
             "image":"wallposter.webp",
             "description":"asthetic hoome|wall deceration  |home decors ",
             "price":"390",
             "categories":"decors"
             },
+
          {
+         "id":9,
          "name":"sneakers",
          "image":"sneakers.webp",
          "description":"white sneakers|better grip  |best quality ",
          "price":"999",
          "categories":"mens_fashion"
         },
-        {
+
+        {   
+            "id":10,
             "name":"saliclic acid",
             "image":"saliclic.webp",
             "description":"cleanse the skin|reduce darkspot  |exfoliate the dead cells ",
@@ -75,11 +93,42 @@ products = [
             "categories":"beauty"
             }
     ]
+#-------------------------------------------HOME-----------------------------------------------
 
 @app.route("/")
 def home():
-    return render_template("home.html" , products=products)
+    return render_template("home.html" , products=products , username = session.get("username"))
 
+#-------------------------------------------login------------------------------------------------
+#----------------------------------------login_page-----------------------------------------------
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        # simple login check
+        if username == "sajin" and password == "1234":
+            session["username"] = username
+            return redirect("/")
+        else:
+            error = "Invalid Username or Password"
+            #return error
+            return render_template("home.html",products=products,error = error , show_login=True, username=session.get("username"))
+    
+     #return render_template("home.html",error = error , show_login=True, username=session.get("username")) redirect()
+
+    
+
+#--------------------------------logout-------------------------------------------------------
+@app.route("/logout")
+def logout():
+    session.pop("username" , None)
+    return redirect("/")
+    
 
 #----------------------------categories--------------------------------
 @app.route("/categories/mens_fashion")
@@ -118,6 +167,27 @@ def beauty():
         if product["categories"] == "beauty":
             filter_products.append(product)
     return render_template("home.html", products=filter_products)
+
+#----------------------------search products-----------------------------------------------------
+@app.route("/search" ,methods=["GET"])
+def search():
+    search = request.args.get("search")
+    result = []
+
+    if search:
+        for product in products:
+            if search.lower() in product["name"].lower() or search.lower() in product["description"].lower():
+                result.append(product)
+        return render_template("home.html", products = result)
+
+
+#------------------------------------------product__page------------------------------------------
+
+@app.route("/product/<int:id>")
+def product_detail(id):
+    for product in products:
+        if product["id"] == id:
+            return render_template("product.html", product=product)
 
 if __name__=="__main__":
     app.run(debug=True)
